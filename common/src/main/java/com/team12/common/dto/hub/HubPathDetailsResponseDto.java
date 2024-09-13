b69a57740863709ef113afd811a41d3900091a89
coding-911 <ye22im@gmail.com>
2024-09-13 17:12:22 +0900

    Common HubPathDetailsResponseDto 구현

--- common/src/main/java/com/team12/common/dto/hub/HubPathDetailsResponseDto.java
+++ common/src/main/java/com/team12/common/dto/hub/HubPathDetailsResponseDto.java
@@ -1,4 +1,14 @@
 package com.team12.common.dto.hub;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class HubPathDetailsResponseDto {
+    private UUID hubPathId;
+    private UUID fromHubId;
+    private UUID toHubId;
+    private double distance;
+    private int duration;
 }
