3845ee0eab04a97ed1e79c24005cf919976a588c
coding-911 <ye22im@gmail.com>
2024-09-16 21:44:35 +0900

    Auditing 어노테이션 추가

--- hub/src/main/java/com/team12/hub/HubApplication.java
+++ hub/src/main/java/com/team12/hub/HubApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing
 @EnableFeignClients
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HubApplication {
