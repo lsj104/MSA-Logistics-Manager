12a22bd8e365fbd381adab8bfd9cd45db56caf01
lsj104 <lsuj104@naver.com>
2024-09-19 01:16:44 +0900

    FEAT/ 주문 CRUD 구현

--- order-delivery/src/main/java/com/team12/order_delivery/order/controller/OrderController.java
+++ order-delivery/src/main/java/com/team12/order_delivery/order/controller/OrderController.java
@@ -3,16 +3,33 @@ package com.team12.order_delivery.order.controller;
 
 import static com.team12.common.exception.response.SuccessResponse.success;
 import static com.team12.order_delivery.order.message.SuccessMessage.CREATE_ORDER;
+import static com.team12.order_delivery.order.message.SuccessMessage.DELETE_ORDER;
+import static com.team12.order_delivery.order.message.SuccessMessage.GET_ORDER;
+import static com.team12.order_delivery.order.message.SuccessMessage.UPDATE_ORDER;
 
+import com.team12.common.customPage.CustomPageResponse;
 import com.team12.common.exception.response.SuccessResponse;
 import com.team12.order_delivery.order.dto.request.CreateOrderRequestDto;
+import com.team12.order_delivery.order.dto.request.UpdateOrderRequestDto;
 import com.team12.order_delivery.order.dto.response.CreateOrderResponseDto;
+import com.team12.order_delivery.order.dto.response.DeleteOrderResponseDto;
+import com.team12.order_delivery.order.dto.response.GetOrderResponseDto;
+import com.team12.order_delivery.order.dto.response.UpdateOrderResponseDto;
 import com.team12.order_delivery.order.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +42,6 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    // TODO: 주문할때마다 수량 줄어들게 설정하기
     @Operation(summary = "주문 생성")
     @PostMapping
     public ResponseEntity<SuccessResponse<CreateOrderResponseDto>> createOrder(
@@ -38,4 +54,57 @@ public class OrderController {
 
     }
 
+    @Operation(summary = "주문 상세 조회")
+    @GetMapping("{orderId}")
+    public ResponseEntity<SuccessResponse<GetOrderResponseDto>> getOrder(
+            @PathVariable("orderId") UUID orderId) {
+        GetOrderResponseDto responseDto = orderService.getOrder(orderId);
+
+        return ResponseEntity.status(GET_ORDER.getHttpStatus())
+                .body(success(GET_ORDER.getHttpStatus().value(),
+                        GET_ORDER.getMessage(), responseDto));
+    }
+
+
+    @Operation(summary = "모든 주문 조회")
+    @GetMapping
+    public ResponseEntity<SuccessResponse<CustomPageResponse<GetOrderResponseDto>>> getAllOrder(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<GetOrderResponseDto> responseDto = orderService.getAllOrder(pageable);
+
+        CustomPageResponse<GetOrderResponseDto> customPageResponse = new CustomPageResponse<>(
+                responseDto);
+
+        return ResponseEntity.status(GET_ORDER.getHttpStatus())
+                .body(success(GET_ORDER.getHttpStatus().value(),
+                        GET_ORDER.getMessage(), customPageResponse));
+    }
+
+    @Operation(summary = "주문 수정")
+    @PutMapping("/{orderId}")
+    public ResponseEntity<SuccessResponse<UpdateOrderResponseDto>> updateOrder(
+            @PathVariable("orderId") UUID orderId,
+            @RequestBody UpdateOrderRequestDto requestDto) {
+
+        UpdateOrderResponseDto responseDto = orderService.updateOrder(requestDto, orderId);
+
+        return ResponseEntity.status(UPDATE_ORDER.getHttpStatus())
+                .body(success(UPDATE_ORDER.getHttpStatus().value(),
+                        UPDATE_ORDER.getMessage(), responseDto));
+    }
+
+    @Operation(summary = "주문 삭제")
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<SuccessResponse<DeleteOrderResponseDto>> deleteOrder(
+            @PathVariable("orderId") UUID orderId) {
+
+        Long userId = null;
+        DeleteOrderResponseDto responseDto = orderService.deleteOrder(orderId, userId);
+
+        return ResponseEntity.status(DELETE_ORDER.getHttpStatus())
+                .body(success(DELETE_ORDER.getHttpStatus().value(),
+                        DELETE_ORDER.getMessage(), responseDto));
+
+    }
 }
--- order-delivery/src/main/java/com/team12/order_delivery/order/domain/Order.java
+++ order-delivery/src/main/java/com/team12/order_delivery/order/domain/Order.java
@@ -2,7 +2,9 @@ package com.team12.order_delivery.order.domain;
 
 import com.team12.common.audit.AuditingEntity;
 import com.team12.order_delivery.delivery.domain.Delivery;
+import com.team12.order_delivery.order.dto.request.UpdateOrderRequestDto;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,18 +36,44 @@ public class Order extends AuditingEntity {
     @Column(name = "product_id", nullable = false)
     private String productId;
 
-    private Order(String producerCompanyId, String receiverCompanyId, Long quantity, String productId) {
+    private Order(String producerCompanyId, String receiverCompanyId, Long quantity,
+            String productId) {
         this.producerCompanyId = producerCompanyId;
         this.receiverCompanyId = receiverCompanyId;
         this.quantity = quantity;
         this.productId = productId;
     }
 
-    public static Order createOrder(String producerCompanyId, String receiverCompanyId, Long quantity, String productId) {
+    public static Order createOrder(String producerCompanyId, String receiverCompanyId,
+            Long quantity, String productId) {
         return new Order(producerCompanyId, receiverCompanyId, quantity, productId);
     }
 
     public void assignDelivery(Delivery delivery) {
         this.delivery = delivery;
+    }
+
+    // 주문 수정
+    public void update(UpdateOrderRequestDto requestDto) {
+        this.producerCompanyId = requestDto.producerCompany() != null ? requestDto.producerCompany()
+                : this.producerCompanyId;
+        this.receiverCompanyId = requestDto.receiverCompany() != null ? requestDto.receiverCompany()
+                : this.receiverCompanyId;
+        this.quantity = requestDto.quantity() != null ? requestDto.quantity() : this.quantity;
+
+        if (this.delivery != null) {
+            this.delivery.updateDeliveryInfo(
+                    requestDto.address(),
+                    requestDto.receiver(),
+                    requestDto.receiverEmail()
+            );
+        }
+    }
+
+    // 주문 삭제
+    public void delete(Long userId) {
+        super.setIsDeleted(true);
+        super.setDeletedAt(LocalDateTime.now());
+        super.setDeletedBy(userId);
     }
 }
