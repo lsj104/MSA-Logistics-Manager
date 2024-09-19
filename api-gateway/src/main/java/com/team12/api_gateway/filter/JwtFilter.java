43993ae85e46e4f2b9229cc8608b46bb127268bd
ryehwa <rhk415@gmail.com>
2024-09-19 18:13:23 +0900

    Header 정보 추가

--- api-gateway/src/main/java/com/team12/api_gateway/filter/JwtFilter.java
+++ api-gateway/src/main/java/com/team12/api_gateway/filter/JwtFilter.java
@@ -1,17 +1,24 @@
 package com.team12.api_gateway.filter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import javax.crypto.SecretKey;
+
 @Component
+@Slf4j
 public class JwtFilter implements GlobalFilter {
 
     @Value("${jwt.key}")
@@ -20,15 +27,16 @@ public class JwtFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
-        if (path.equals("/api/auth/login")) {
+        if (path.equals("/api/auth/login") || path.equals("/api/users/sign-up")) {
             return chain.filter(exchange);  // /signIn 경로는 필터를 적용하지 않음
         }
 
         String token = extractToken(exchange);
 
-        if (token == null || !validateToken(token)) {
+        if (token == null || !validateToken(token, exchange)) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
+            log.info(exchange.getRequest().getURI().getPath());
+            return onError(exchange, "유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED);
         }
 
         return chain.filter(exchange);
@@ -42,13 +50,42 @@ public class JwtFilter implements GlobalFilter {
         return null;
     }
 
-    private boolean validateToken(String token) {
+    private boolean validateToken(String token, ServerWebExchange exchange) {
         try {
             Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
             return false;
         }
+//        try {
+//            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+//            Jws<Claims> claimsJws = Jwts.parserBuilder()
+//                    .setSigningKey(key)
+//                    .build()
+//                    .parseClaimsJws(token);
+//            Claims claims = claimsJws.getBody();
+//            String userId = claims.get("userId", String.class);
+//            String username = claims.get("username", String.class);
+//            String role = claims.get("role", String.class);
+//            exchange.getRequest().mutate()
+//                    .header("X-User-Id", userId)
+//                    .header("X-User-Name",username)
+//                    .header("X-User-Role", role)
+//                    .build();
+//            return true;
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//            return false;
+//        }
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String errorMsg, HttpStatus status) {
+        exchange.getResponse().setStatusCode(status);
+        exchange.getResponse().getHeaders().add("Content-Type", "application/json");
+        String body = "{\"error\": \"" + errorMsg + "\"}";
+        return exchange.getResponse().writeWith(Mono.just(exchange.getResponse()
+                .bufferFactory()
+                .wrap(body.getBytes())));
     }
 }
 
